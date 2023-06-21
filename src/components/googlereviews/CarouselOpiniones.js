3aa0edd08c6b8a8b6d9d50df63d5cad6103adc27
David Barba <david.barbapedreno@metronom.com>
2023-06-21 17:09:48 +0200

    Google Review section finished

--- src/components/googlereviews/CarouselOpiniones.js
+++ src/components/googlereviews/CarouselOpiniones.js
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import { useTranslation, Trans } from 'react-i18next';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+
 import { makeStyles } from '@mui/styles';
 
+import review1 from '../../images/opiniones/review1.png'
+import review1EN from '../../images/opiniones/review1EN.png'
+import review1FR from '../../images/opiniones/review1FR.png'
+import review2 from '../../images/opiniones/review2.png'
+import review2EN from '../../images/opiniones/review2EN.png'
+import review2FR from '../../images/opiniones/review2FR.png'
+import review3 from '../../images/opiniones/review3.png'
+import review3EN from '../../images/opiniones/review3EN.png'
+import review3FR from '../../images/opiniones/review3FR.png'
+import review4 from '../../images/opiniones/review4.png'
+import review4EN from '../../images/opiniones/review4EN.png'
+import review4FR from '../../images/opiniones/review4FR.png'
+import review5 from '../../images/opiniones/review5.png'
+import review5EN from '../../images/opiniones/review5EN.png'
+import review5FR from '../../images/opiniones/review5FR.png'
+import review6 from '../../images/opiniones/review6.png'
+import review6EN from '../../images/opiniones/review6EN.png'
+import review6FR from '../../images/opiniones/review6FR.png'
+import review7 from '../../images/opiniones/review7.png'
+import review7EN from '../../images/opiniones/review7EN.png'
+import review7FR from '../../images/opiniones/review7FR.png'
+import review8 from '../../images/opiniones/review8.png'
+import review8EN from '../../images/opiniones/review8EN.png'
+import review8FR from '../../images/opiniones/review8FR.png'
 
-import { useTranslation, Trans } from 'react-i18next';
 
-import pazEasySailing from '../../images/cursos/pazEasySailing.jpeg'
-import PazCursoEasySailing from '../../images/cursos/PazCursoEasySailing.jpg'
-
-import draftReview1 from './draftReview1.png'
-import draftReview2 from './draftReview2.png'
-import draftReview3 from './draftReview3.png'
 
 const useStyles = makeStyles((theme) => ({
 	imagenes: {
@@ -23,15 +41,20 @@ const useStyles = makeStyles((theme) => ({
 			width:'105%',
 		},
 	},
+	slider: {
+		// border: '1px solid #555',
+	}
 }));
 
 
 const CarouselOpiniones = () => {
 
-	const classes = useStyles();
-	const [language, setLanguage] = useState('es');
-
 	const { t, i18n } = useTranslation();
+	const classes = useStyles();
+	const [language, setLanguage] = useState(i18n.language.substring(0,2));
+
+	console.log('language is: '+language);
+	console.log('language is: '+t('idioma'));
 
 	useEffect(() => {
 		const newLanguage = i18n.language.substring(0,2);
@@ -48,7 +71,7 @@ const CarouselOpiniones = () => {
 		slidesToScroll: 1,
 		arrows: false,
 		autoplay: true,
-		autoplaySpeed: 5000,
+		autoplaySpeed: 4000,
 		// rows: 2,
 		// height: '300px'
 		// nextArrow: <ArrowForwardIos color="action" fontSize="large"/>,
@@ -71,42 +94,51 @@ const CarouselOpiniones = () => {
 
 	return (
 		<a href="https://search.google.com/local/reviews?placeid=ChIJk0CT-LQ7Yg0RWP6OWELO3_Q" target="_blank" rel="noopener">
-		<Slider {...settings}>
-			{/*<div>*/}
-			{/*	<img src="foto1.jpg" alt="Foto 1" />*/}
-			{/*</div>*/}
-			{/*<div>*/}
-			{/*	<img src="foto2.jpg" alt="Foto 2" />*/}
-			{/*</div>*/}
-			{/*<div>*/}
-			{/*	<img src="foto3.jpg" alt="Foto 3" />*/}
-			{/*</div>*/}
-			{/*<div>*/}
-			{/*	<img src="foto4.jpg" alt="Foto 4" />*/}
-			{/*</div>*/}
-			{/*<div>*/}
-			{/*	<img src="foto5.jpg" alt="Foto 5" />*/}
-			{/*</div>*/}
+		<Slider {...settings} className={classes.slider}>
 
 			<div>
-				{/*<img src={draftReview1} alt="Foto 1" style={{width:'100%', height: 'auto'}} />*/}
-				<img src={draftReview1} alt="Foto 1" className={classes.imagenes} />
+				{ t('idioma') === 'es' && <img src={review1} alt="Review 1" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review1FR} alt="Review 1" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review1EN} alt="Review 1" className={classes.imagenes} /> }
 			</div>
 			<div>
-				<img src={draftReview1} alt="Foto 2" className={classes.imagenes} />
+				{ t('idioma') === 'es' && <img src={review2} alt="Review 2" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review2FR} alt="Review 2" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review2EN} alt="Review 2" className={classes.imagenes} /> }
 			</div>
 			<div>
-				<img src={draftReview1} alt="Foto 3" className={classes.imagenes} />
+				{ t('idioma') === 'es' && <img src={review3} alt="Review 3" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review3FR} alt="Review 3" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review3EN} alt="Review 3" className={classes.imagenes} /> }
 			</div>
 			<div>
-				<img src={draftReview1} alt="Foto 4" className={classes.imagenes} />
+				{ t('idioma') === 'es' && <img src={review4} alt="Review 4" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review4FR} alt="Review 4" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review4EN} alt="Review 4" className={classes.imagenes} /> }
 			</div>
 			<div>
-				<img src={draftReview1} alt="Foto 5" className={classes.imagenes} />
+				{ t('idioma') === 'es' && <img src={review5} alt="Review 5" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review5FR} alt="Review 5" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review5EN} alt="Review 5" className={classes.imagenes} /> }
+			</div>
+			<div>
+				{ t('idioma') === 'es' && <img src={review6} alt="Review 6" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review6FR} alt="Review 6" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review6EN} alt="Review 6" className={classes.imagenes} /> }
+			</div>
+			<div>
+				{ t('idioma') === 'es' && <img src={review7} alt="Review 7" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review7FR} alt="Review 7" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review7EN} alt="Review 7" className={classes.imagenes} /> }
+			</div>
+			<div>
+				{ t('idioma') === 'es' && <img src={review8} alt="Review 8" className={classes.imagenes} /> }
+				{ t('idioma') === 'fr' && <img src={review8FR} alt="Review 8" className={classes.imagenes} /> }
+				{ t('idioma') === 'en' && <img src={review8EN} alt="Review 8" className={classes.imagenes} /> }
 			</div>
 			{/* Agrega más imágenes aquí */}
 		</Slider>
-		<div style={{height: '150px'}} />
+		{/*<div style={{height: '150px'}} />*/}
 		</a>
 	);
 };
--- src/components/googlereviews/GoogleReviews.js
+++ src/components/googlereviews/GoogleReviews.js
@@ -1,66 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { useTranslation, Trans } from 'react-i18next';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-import {Grid} from "@mui/material";
-import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 
-import AnchorSpan from "../separadores/AnchorSpan";
 import GoogleBadge from "./GoogleBadge";
 import CarouselOpiniones from "./CarouselOpiniones";
 
+import resenaVerificada from '../../images/opiniones/google-resenas-verificadas.png'
+import verifiedReviewsLogo from '../../images/opiniones/verifiedReview.png'
+import googleAvisClients from '../../images/opiniones/googleAvisClients.png'
+
+import escriberesena from '../../images/opiniones/escribeResena.png'
+import reviewUsGoogle from '../../images/opiniones/reviewUsGoogle.png'
+import reviewUsFrench from '../../images/opiniones/donnezVotreAvisGoogle.png'
+
+
 
 const useStyles = makeStyles((theme) => ({
-	tituloCursos: {
+	logoResenas: {
 		display: "flex",
 		justifyContent: "center",
-		marginBottom: '10px',
+		// marginBottom: '10px',
 		width: '100%',
 	},
-	horizontalLine: {
+	divResena: {
 		display: "flex",
 		justifyContent: "center",
-		width: '100px',
-		height: '3px',
-		backgroundColor: '#CF9500',
-		marginBottom: '40px',
+		// marginBottom: '10px',
+		width: '100%',
+		marginTop: '20px',
+		marginBottom: '20px',
 	},
-	tituloTexto: {
-		// fontFamily: 'Cinzel',
-		fontFamily: 'Reem Kufi Fun',
-		fontWeight: '700',
+	// horizontalLine: {
+	// 	display: "flex",
+	// 	justifyContent: "center",
+	// 	width: '100px',
+	// 	height: '3px',
+	// 	backgroundColor: '#CF9500',
+	// 	marginBottom: '40px',
+	// },
+	fondo: {
+		// width: '100%',
+		backgroundColor: '#f8f8f8',
 	},
+	resenaImage: {
+		width: '20%',
+		height: 'auto',
+	},
+	writeResenaImage: {
+		width: '15%',
+		height: '100%',
+	},
+
+
 }));
 
 const GoogleReviews = () => {
 
 	const classes = useStyles();
-	const { t, i18n } = useTranslation();
+	const { t } = useTranslation();
 
 
 	return (
+		<div className={classes.fondo}>
+
 		<Grid container>
 			<Grid item xs={12} sm={12} md={12} >
-				<div className={classes.tituloCursos}>
-					<Typography variant="h4" className={classes.tituloTexto}>
-						{/*{' ? Qué dicen de nosotros ?'}*/}
-						{t('opiniones.titulo')}
-					</Typography>
-				</div>
-			</Grid>
-			<Grid item xs={12} sm={12} md={12} >
-				<div className={classes.tituloCursos}>
-					<div className={classes.horizontalLine} />
+				<div className={classes.logoResenas}>
+					{ t('idioma') === 'es' && <img src={resenaVerificada} alt="opiniones" className={classes.resenaImage} />}
+					{ t('idioma') === 'en' && <img src={verifiedReviewsLogo} alt="opiniones" className={classes.resenaImage} /> }
+					{ t('idioma') === 'fr' && <img src={googleAvisClients} alt="opiniones" className={classes.resenaImage} /> }
 				</div>
 			</Grid>
 
-			<Grid item xs={12} sm={12} md={12} >
-				<GoogleBadge />
+			<Grid item xs={12} sm={12} md={12} style={{paddingBottom: '1em'}}>
+				<GoogleBadge/>
 			</Grid>
-			<Grid item xs={12} sm={12} md={12} >
+
+			<Grid item xs={1} sm={1} md={1} />
+			<Grid item xs={10} sm={10} md={10} >
 				<CarouselOpiniones />
 			</Grid>
-		</Grid>
+			<Grid item xs={1} sm={1} md={1} />
+
+			<Grid item xs={12} sm={12} md={12} >
+				<div className={classes.divResena} >
+					{ t('idioma') === 'es' && <img src={escriberesena} alt="resena" className={classes.writeResenaImage} />}
+					{ t('idioma') === 'en' && <img src={reviewUsGoogle} alt="resena" className={classes.writeResenaImage} />}
+					{ t('idioma') === 'fr' && <img src={reviewUsFrench} alt="resena" className={classes.writeResenaImage} />}
+				</div>
+			</Grid>
+
+			</Grid>
+		</div>
 	)
 };
 
