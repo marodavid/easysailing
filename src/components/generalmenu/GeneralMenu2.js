c9d8ed5b9bad91907fd2cf7e6f5bab5e5eee0c39
David Barba <david.barbapedreno@metronom.com>
2023-06-23 14:59:28 +0200

    Draft GeneralMenu2

--- src/components/generalmenu/GeneralMenu2.js
+++ src/components/generalmenu/GeneralMenu2.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { GB } from 'country-flag-icons/react/3x2'
+import { HashLink } from 'react-router-hash-link';
+
+import { useTranslation, Trans } from 'react-i18next';
 import {
 	Grid,
 	Link,
@@ -42,7 +44,7 @@ const Typography1 = styled(Typography)(({ theme }) => ({
 	flex: 1,
 	display: 'flex',
 	[theme.breakpoints.down('md')]: {
-		fontSize: 20
+		fontSize: 15
 	}
 }));
 const Typography2 = styled(Typography)(({ theme }) => ({
@@ -51,11 +53,13 @@ const Typography2 = styled(Typography)(({ theme }) => ({
 	textDecoration: 'none',
 	// fontStyle: 'cursive',
 	// color: theme.palette.white,
+	textTransform: 'uppercase',
 	color: '#02006c',
 	underline: false,
 	justifyContent: 'center',
 	flex: 1,
 	display: 'flex',
+	// borderStyle: 'double',
 	[theme.breakpoints.down('md')]: {
 		fontSize: 15
 	}
@@ -65,10 +69,9 @@ const MenuLink = styled(Link)(({ theme }) => ({
 	textDecoration: 'none'
 }));
 
-
-
-
-
+const MenuHashLink = styled(Link)(({ theme }) => ({
+	textDecoration: 'none'
+}));
 
 const DivPlaceholder = styled('div')(({ theme }) => ({
 	// height: 164,
@@ -112,6 +115,9 @@ const DivRight = styled('div')(({ theme }) => ({
 	flex: 1,
 	display: 'flex',
 	justifyContent: 'flex-end',
+	// [theme.breakpoints.down('md')]: {
+	// 	display: 'none',
+	// }
 }));
 
 const LinkRight = styled(Link)(({ theme }) => ({
@@ -125,6 +131,10 @@ const LogoIMG = styled('img')(({ theme }) => ({
 	height: '4.5em',
 	// marginLeft: theme.spacing(1),
 	marginRight: theme.spacing(1),
+	[theme.breakpoints.down('md')]:{
+		width: '3em',
+		height: '3em',
+	}
 }));
 
 const FacebookIconSize = styled(FacebookIcon)(({ theme}) => ({
@@ -148,6 +158,19 @@ const WhatsAppIconSize = styled(WhatsAppIcon)(({ theme}) => ({
 		fontSize: 25,
 	}
 }));
+
+const DivHorizontalLine = styled('div')(({ theme}) => ({
+		display: "flex",
+		justifyContent: "center",
+		width: '100%',
+		height: '1px',
+		// backgroundColor: '#CF9500',
+		backgroundColor: 'black',
+		marginBottom: '40px',
+	[theme.breakpoints.up('md')]:{
+		display: 'none'
+	}
+}));
 // const LinkLeft = styled(Link)(({ theme }) => ({
 //   color: theme.palette.white,
 //   underline: false,
@@ -166,17 +189,25 @@ const ImagenLogoLeft = styled('img')(({ theme }) => ({
 	}
 }));
 
-// const FlagIcons = styled('svg')(( { theme }) => ({
-const FlagIcons = styled(GB)(( { theme }) => ({
-	userSelect: 'none',
-	width: '1em',
-	height: '1em',
-	display: 'inline-block',
-	fill: 'currentColor',
-	flexShrink: 0,
-	transition: 'fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-	fontSize: '2.1875rem',
-}))
+const DivFondo = styled('div')(({ theme }) => ({
+	backgroundColor: '#f8f8f8',
+	width: '100%',
+	[theme.breakpoints.down('md')]:{
+		display: 'none'
+	}
+}));
+
+
+// const FlagIcons = styled(GB)(( { theme }) => ({
+// 	userSelect: 'none',
+// 	width: '1em',
+// 	height: '1em',
+// 	display: 'inline-block',
+// 	fill: 'currentColor',
+// 	flexShrink: 0,
+// 	transition: 'fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+// 	fontSize: '2.1875rem',
+// }))
 
 // const Transition = React.forwardRef(function Transition(props, ref) {
 //   return <Slide direction="up" ref={ref} {...props} />;
@@ -184,7 +215,9 @@ const FlagIcons = styled(GB)(( { theme }) => ({
 
 function GeneralMenu2(props) {
 	const { window } = props
-	const { colorMenu } = props
+	// const { colorMenu } = props
+	const { t, i18n } = useTranslation();
+
 
 	const trigger = useScrollTrigger({
 		disableHysteresis: true,
@@ -204,10 +237,11 @@ function GeneralMenu2(props) {
 							<Grid item xs={1} sm={1} md={2} lg={2} >
 								<MenuOpciones />
 							</Grid>
-							<Grid item xs={1} sm={1} md={2} lg={2} />
+							<Grid item xs={0.25} sm={0.25} md={2} lg={2} />
 
 							{/*<Grid item xs={11} sm={11} md={9} lg={9} />*/}
-							<Grid item xs={8} sm={8} md={4} lg={4}>
+							{/*<Grid item xs={6} sm={6} md={4} lg={4}>*/}
+							<Grid item xs={4.75} sm={4.75} md={4} lg={4}>
 								<Typography1>
 									{'Easy Sailing'}
 								</Typography1>
@@ -219,6 +253,8 @@ function GeneralMenu2(props) {
 								<LogoIMG src={ESLogo} alt={''} />
 							</Grid>
 							<Grid item xs={1} sm={1} md={1} lg={1} />
+
+
 							{/*<Grid item xs={4} sm={4} md={2} lg={2}>*/}
 							{/*	<DivCenter>*/}
 							{/*		<Typography2>*/}
@@ -227,7 +263,7 @@ function GeneralMenu2(props) {
 							{/*	</DivCenter>*/}
 							{/*</Grid>*/}
 							{/*<Grid item xs={1} sm={1} md={1} lg={1} />*/}
-							<Grid item xs={2} sm={2} md={2} lg={2}>
+							<Grid item xs={4} sm={4} md={2} lg={2}>
 								<DivRight>
 									<LinkRight
 										variant="h6"
@@ -274,51 +310,69 @@ function GeneralMenu2(props) {
 								</DivRight>
 							</Grid>
 
-
+							<Grid item xs={12} sm={12} md={12} lg={12}>
+								<DivHorizontalLine />
+							</Grid>
 
 							<Grid item xs={12} sm={12} md={12} lg={12}>
 								<div style={{ height: '1em' }} />
 							</Grid>
 
+							<Grid item xs={12} sm={12} md={12} lg={12}>
+								<DivFondo>
+									<Grid container row justify="center" alignItems="center">
+
+
+
+
+
 							<Grid item xs={4} sm={4} md={2} lg={2}>
 								<MenuLink onClick={() => alert('proximamente')}>
 									<Typography2>
-										{'QUIENES SOMOS'}
+										{/*{'QUIENES SOMOS'}*/}
+										{t('menuOpciones.whoweare')}
 									</Typography2>
 								</MenuLink>
 							</Grid>
 							<Grid item xs={4} sm={4} md={2} lg={2}>
-								<MenuLink onClick={() => alert('proximamente')}>
+								{/*<MenuLink onClick={() => alert('proximamente')}>*/}
+								{/*<HashLink onClick={() => alert('proximamente')}>*/}
+								<HashLink to='/#cursos' style={{textDecoration: 'none'}}>
 								<Typography2>
-									{'CURSOS'}
+									{/*{'CURSOS'}*/}
+									{t('menuOpciones.cursos')}
 								</Typography2>
-								</MenuLink>
+								</HashLink>
 							</Grid>
 							<Grid item xs={4} sm={4} md={2} lg={2}>
 								<MenuLink onClick={() => alert('proximamente')}>
 								<Typography2>
-									{'ALQUILERES'}
+									{/*{'ALQUILERES'}*/}
+									{t('menuOpciones.alquileres')}
 								</Typography2>
 								</MenuLink>
 							</Grid>
 							<Grid item xs={4} sm={4} md={2} lg={2}>
 								<MenuLink onClick={() => alert('proximamente')}>
 									<Typography2>
-									{'EXPERIENCIAS'}
+									{/*{'EXPERIENCIAS'}*/}
+									{t('menuOpciones.actividades')}
 								</Typography2>
 								</MenuLink>
 							</Grid>
 							<Grid item xs={4} sm={4} md={2} lg={2}>
 								<MenuLink onClick={() => alert('proximamente')}>
 								<Typography2>
-									{'COLABORACIONES'}
+									{/*{'COLABORACIONES'}*/}
+									{t('menuOpciones.colaboraciones')}
 								</Typography2>
 								</MenuLink>
 							</Grid>
 							<Grid item xs={4} sm={4} md={2} lg={2}>
 								<MenuLink onClick={() => alert('proximamente')}>
 									<Typography2>
-									{'CONTACTO'}
+									{/*{'CONTACTO'}*/}
+									{t('menuOpciones.contacto')}
 								</Typography2>
 								</MenuLink>
 							</Grid>
@@ -332,7 +386,12 @@ function GeneralMenu2(props) {
 								</div>
 							</Grid>
 						</Grid>
-					</ToolbarStyles>
+								</DivFondo>
+							</Grid>
+
+					</Grid>
+
+				</ToolbarStyles>
 				</AppBar>
 				<DivPlaceholder />
 			</DivRoot>
@@ -354,7 +413,7 @@ function GeneralMenu2(props) {
 }
 
 GeneralMenu2.propTypes = {
-	colorMenu: PropTypes.string.isRequired,
+	// colorMenu: PropTypes.string.isRequired,
 };
 
 export default GeneralMenu2;
