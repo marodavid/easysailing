a79b700f1e9479adca5cfc6fde3a2ebab25cdc38
David Barba <david.barbapedreno@metronom.com>
2023-06-23 14:54:48 +0200

    Draft Licencia Navegacion

--- src/components/cursos/LicenciaNavegacion.js
+++ src/components/cursos/LicenciaNavegacion.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@mui/styles';
+import React from 'react';
+import {styled} from "@mui/system"
+
 import { Button, Grid, Typography } from '@mui/material';
 import { Dialog, DialogTitle,  } from '@mui/material';
-import { useTranslation, Trans } from 'react-i18next';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSailboat } from '@fortawesome/free-solid-svg-icons'
-import GeneralMenu2 from "../generalmenu/GeneralMenu2";
+import { useTranslation } from 'react-i18next';
 
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
@@ -19,11 +16,13 @@ import 'dayjs/locale/fr';
 
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSailboat, faCompassDrafting, faSun } from '@fortawesome/free-solid-svg-icons'
+import jetSki from '../../images/cursos/licencianavegacion/jetski.png'
+import helice from '../../images/cursos/licencianavegacion/helice.png'
 
+import GeneralMenu2 from "../generalmenu/GeneralMenu2";
 
-
-import pazEasySailing from '../../images/cursos/pazEasySailing.jpeg'
-import PazCursoEasySailing from '../../images/cursos/PazCursoEasySailing.jpg'
 import VentanaVideo from "../ventanavideo/VentanaVideo";
 import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
@@ -31,75 +30,79 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContentText from "@mui/material/DialogContentText";
 import {getTodayDate} from "@mui/x-date-pickers/internals";
 
-const useStyles = makeStyles((theme) => ({
-	card: {
-		transition: 'transform 0.3s',
-		'&:hover': {
-			transform: 'scale(1.05)',
-		},
-	},
 
-	button: {
-		display: "flex",
-		justifyContent: "center",
-		marginBottom: '10px',
-		marginTop: '15px',
-		width: '100%',
-	},
-
-	tituloCursos: {
-		display: "flex",
-		justifyContent: "left",
-		marginBottom: '10px',
-		marginTop: '15px',
-		width: '100%',
-	},
-
-	tituloTexto: {
-		// fontFamily: 'Cinzel',
-		fontFamily: 'Reem Kufi Fun',
-		fontWeight: '700',
-	},
-
-	tituloBulletpoint: {
-		// fontFamily: 'Cinzel',
-		fontFamily: 'Reem Kufi Fun',
-		// fontFamily: "Roboto Helvetica Arial sans-serif",
-		fontWeight: '300',
-		fontSize: '21px',
-	},
-	textoBulletpoint: {
-		// fontFamily: 'Cinzel',
-		// fontFamily: 'Reem Kufi Fun',
-		fontFamily: "Roboto Helvetica Arial sans-serif",
-		fontWeight: '300',
-		fontSize: '19px',
-	},
-
-	horizontalLine: {
-		display: "flex",
-		justifyContent: "center",
-		width: '100px',
-		height: '3px',
-		backgroundColor: '#CF9500',
-		marginBottom: '40px',
-
-	},
-	separator: {
-		height: '10px',
-	},
-	separatorBulletPoint: {
-		height: '30px',
-	},
-	separatorBetweenPoint: {
-		height: '20px',
-	},
-	bigSeparator: {
-		height: '100px',
-	},
-
-
+const DivBoton = styled('div')(({ theme }) => ({
+	display: "flex",
+	justifyContent: "center",
+	marginBottom: '10px',
+	marginTop: '15px',
+	width: '100%',
 }));
+
+const DivTituloCursos = styled('div')(({ theme }) => ({
+	display: "flex",
+	justifyContent: "left",
+	marginBottom: '10px',
+	marginTop: '15px',
+	width: '100%',
+}));
+
+const SpanTituloBulletpoint = styled('div')(({ theme }) => ({
+	display: "flex",
+	justifyContent: "left",
+	marginBottom: '10px',
+	marginTop: '15px',
+	width: '100%',
+}));
+
+const DivHorizontalLine = styled('div')(({ theme }) => ({
+	display: "flex",
+	justifyContent: "center",
+	width: '100px',
+	height: '3px',
+	backgroundColor: '#CF9500',
+	marginBottom: '20px',
+}));
+
+const DivSeparator = styled('div')(({ theme }) => ({
+	height: '1em',
+}));
+
+const DivSeparatorBulletpoint = styled('div')(({ theme }) => ({
+	height: '1em',
+}));
+
+const DivBigSeparator = styled('div')(({ theme }) => ({
+	height: '3em',
+}));
+
+const ImagenLogoJetski = styled('img')(({ theme }) => ({
+	width: '80%',
+	height: 'auto',
+	marginLeft: '-1.5em',
+	[theme.breakpoints.down("xl")]: {
+		width: '95%',
+		marginLeft: '-0.5em',
+
+	},
+}));
+
+const ImagenLogoHelice = styled('img')(({ theme }) => ({
+	width: '50%',
+	height: 'auto',
+	marginLeft: '-0.5em',
+	[theme.breakpoints.down("xl")]: {
+		width: '70%',
+		marginLeft: '0em',
+	}
+}));
+
+const PrimerVideo = styled('span')(({ theme }) => ({
+	[theme.breakpoints.down("lg")]: {
+			display: 'none',
+	}
+}));
+
 
 const LicenciaNavegacion = () => {
 
@@ -112,8 +115,8 @@ const LicenciaNavegacion = () => {
 	const handleClose = () => {
 		setOpen(false);
 	};
-	const classes = useStyles();
-	const {t, i18n} = useTranslation();
+
+	const { t } = useTranslation();
 
 	return (
 		<>
@@ -123,7 +126,7 @@ const LicenciaNavegacion = () => {
 				<Grid item xs={1} sm={1} md={2} lg={2} />
 				<Grid item xs={10} sm={10} md={8} lg={8} >
 					{/*<div className={classes.tituloCursos} style={{alignItems: "left"}}>*/}
-						<Typography variant="h3" className={classes.tituloTexto} align={'left'}  >
+						<Typography variant="h3" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 							{/*{' Cursos y Licencias'}*/}
 							{ t('cursos.LicenciaNavegacion.Titulo')}
 						</Typography>
@@ -133,142 +136,259 @@ const LicenciaNavegacion = () => {
 
 				<Grid item xs={1} sm={1} md={2} lg={2} />
 				<Grid item xs={10} sm={10} md={8} lg={8} >
-					<div className={classes.tituloCursos}>
-						<div className={classes.horizontalLine} />
-					</div>
+					<DivTituloCursos>
+						<DivHorizontalLine />
+						{/*<div className={classes.horizontalLine} />*/}
+					</DivTituloCursos>
 				</Grid>
 				<Grid item xs={1} sm={1} md={2} lg={2} />
 
 
 
 				<Grid item xs={1} sm={1} md={2} lg={2} />
-				<Grid item xs={8} sm={8} md={5} lg={5} >
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+				{/*<Grid item xs={8} sm={8} md={5} lg={5} >*/}
+				<Grid item xs={10} sm={10} md={5} lg={5} >
+					{/*<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >*/}
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo1')}
 					</Typography>
-					<div className={classes.separator} />
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+					<DivSeparator />
+					{/*<div className={classes.separator} />*/}
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo2')}
 					</Typography>
-					<div className={classes.separator} />
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+					<DivSeparator />
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo3')}
 					</Typography>
 
-					<div className={classes.separator} />
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+					<DivSeparator />
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo4')}
 					</Typography>
-					<div className={classes.separator} />
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+					<DivSeparator />
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo5')}
 					</Typography>
-					<div className={classes.separator} />
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+					<DivSeparator />
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo6')}
 					</Typography>
-					<div className={classes.separator} />
-					<Typography variant="body1" className={classes.tituloTexto} align={'left'}  >
+					<DivSeparator />
+					<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 						{ t('cursos.LicenciaNavegacion.Parrafo7')}
 					</Typography>
-
-
-					</Grid>
-				<Grid item xs={0.25} sm={0.25} md={1} lg={1} />
-				<Grid item xs={2.5} sm={2.5} md={3} lg={3} >
-					{/*<VentanaVideo  embedId='https://player.vimeo.com/video/721928154' />*/}
-					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />
 				</Grid>
-				<Grid item xs={0.25} sm={0.25} md={1} lg={1} />
+
+				<Grid item xs={0.5} sm={0.5} md={0.5} lg={0.5} />
+				<Grid item xs={0.25} sm={0.25} md={4} lg={4} >
+
+				{/*<Grid item xs={0.25} sm={0.25} md={1} lg={0.25} />*/}
+
+				{/*<Grid item xs={2.75} sm={2.75} md={4} lg={4} >*/}
+				{/*<Grid item xs={2.75} sm={2.75} md={5} lg={5} >*/}
+				{/*<Grid item xs={2.5} sm={2.5} md={3} lg={3.75} >*/}
+					{/*<VentanaVideo  embedId='https://player.vimeo.com/video/721928154' />*/}
+					<PrimerVideo>
+						<VentanaVideo  embedId='https://player.vimeo.com/video/838927728?h=d3ef5f63dc' />
+					</PrimerVideo>
+				</Grid>
+				<Grid item xs={0.25} sm={0.25} md={0.5} lg={0.5} />
+
+
+
+				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+				{/*<Grid item xs={10} sm={10} md={3} lg={3} >*/}
+				{/*	<VentanaVideo  embedId='https://player.vimeo.com/video/838914025?' />*/}
+				{/*</Grid>*/}
+				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+
+				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+				{/*<Grid item xs={10} sm={10} md={3} lg={3} >*/}
+				{/*	<VentanaVideo  embedId='https://player.vimeo.com/video/838914025?' />*/}
+				{/*</Grid>*/}
+				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+
+				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+				{/*<Grid item xs={10} sm={10} md={3} lg={3} >*/}
+				{/*	<VentanaVideo  embedId='https://player.vimeo.com/video/838914025?' />*/}
+				{/*</Grid>*/}
+				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+
+
 
 				<Grid item xs={12} sm={12} md={12} lg={12} >
-					<div className={classes.bigSeparator} />
+					<DivBigSeparator />
+					{/*<div className={classes.bigSeparator} />*/}
 				</Grid>
 
 
 				<Grid item xs={1} sm={1} md={2} lg={2} />
 				<Grid item xs={8} sm={8} md={5} lg={5} >
-					<Typography variant="h5" className={classes.tituloTexto} style={{fontWeight: 'bold'}} align={'left'} >
+					<Typography variant="h5" style={{fontFamily: 'Reem Kufi Fun', fontWeight: 'bold'}} align={'left'} >
 						{ t('cursos.LicenciaNavegacion.Bulletpoint.Heading')}
 					</Typography>
 				</Grid>
 				<Grid item xs={3} sm={3} md={5} lg={5} />
 
 				<Grid item xs={12} sm={12} md={12} lg={12} >
-					<div className={classes.separatorBulletPoint} />
+					<DivSeparatorBulletpoint />
+					{/*<div className={classes.separatorBulletPoint} />*/}
 				</Grid>
 
 
-				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
-				<Grid item xs={5} sm={5} md={6} lg={6} >
-					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />
-				</Grid>
-				{/*<Grid item xs={1} sm={1} md={1} lg={1 } />*/}
-				<Grid item xs={5} sm={5} md={6} lg={6} >
-					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />
-				</Grid>
-				{/*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*/}
+				{/*/!*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*!/*/}
+				{/*<Grid item xs={5} sm={5} md={6} lg={6} >*/}
+				{/*	<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />*/}
+				{/*</Grid>*/}
+				{/*/!*<Grid item xs={1} sm={1} md={1} lg={1 } />*!/*/}
+				{/*<Grid item xs={5} sm={5} md={6} lg={6} >*/}
+				{/*	<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />*/}
+				{/*</Grid>*/}
+				{/*/!*<Grid item xs={1} sm={1} md={0.5} lg={0.5} />*!/*/}
+
 
 
 				<Grid item xs={1} sm={1} md={2} lg={2} />
-
-				<Grid item xs={1} sm={1} md={0.75} lg={0.75} >
+				<Grid item xs={2} sm={1} md={0.75} lg={0.75} >
 					<FontAwesomeIcon icon={faSailboat} size="3x" />
-					{/*<span className={classes.tituloBulletpoint}>{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto1Titulo') }</span>*/}
 				</Grid>
+				<Grid item xs={8} sm={3.5} md={2} lg={2} >
+					<SpanTituloBulletpoint align={'left'}>
+					{/*<span className={classes.textoBulletpoint} align={'left'} >*/}
+						<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
 
-				<Grid item xs={3.5} sm={3.5} md={2} lg={2} >
-					{/*<Typography variant="h6" className={classes.tituloBulletpoint} align={'left'} >*/}
-						<span className={classes.tituloBulletpoint}>
-						{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto1Titulo')}
-						{/*{ 'Atribuciones Licencia dee' }*/}
-						</span>
-					<div className={classes.separatorBetweenPoint} />
-					{/*</Typography>*/}
-					<span className={classes.textoBulletpoint} align={'left'} >
 						{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto1Texto')}
-						{/*{ 'Atribuciones Licencia dee' }*/}
-					</span>
-				</Grid>
-				<Grid item xs={1} sm={1} md={1} lg={1} />
-
-				<Grid item xs={3.5} sm={3.5} md={2} lg={2} >
-					<Typography variant="h5" className={classes.tituloTexto} align={'left'} >
-						{ t('cursos.LicenciaNavegacion.Bulletpoint.Heading')}
-					</Typography>
+						</Typography>
+					</SpanTituloBulletpoint>
 				</Grid>
 
-				{/*<iframe src="https://player.vimeo.com/video/835483432?h=8c9cd503a9" width="640" height="564" frameBorder="0"*/}
-				{/*				allow="autoplay; fullscreen" allowFullScreen></iframe>*/}
+				<Grid item xs={1} sm={0.5} md={0.5} lg={0.5} />
+				<Grid item xs={1} sm={0.5} md={0.5} lg={0.5} />
+
+				<Grid item xs={2} sm={1} md={0.75} lg={0.75} >
+					<FontAwesomeIcon icon={faCompassDrafting} size="3x" />
+				</Grid>
+				<Grid item xs={8} sm={3.5} md={2} lg={2} >
+					<SpanTituloBulletpoint align={'left'} >
+					{/*<span className={classes.textoBulletpoint} align={'left'} >*/}
+						<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
+						{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto2Texto')}
+						</Typography>
+					</SpanTituloBulletpoint>
+				</Grid>
+				<Grid item xs={1} sm={1} md={2.5} lg={2.5} />
+
+
+				<Grid item xs={1} sm={1} md={2} lg={2} />
+				<Grid item xs={2} sm={1} md={0.75} lg={0.75} >
+					<FontAwesomeIcon icon={faSun} size="3x" />
+				</Grid>
+				<Grid item xs={8} sm={3.5} md={2} lg={2} >
+					<SpanTituloBulletpoint align={'left'}>
+						<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
+							{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto3Texto')}
+						</Typography>
+					</SpanTituloBulletpoint>
+				</Grid>
+
+				<Grid item xs={1} sm={0.5} md={0.5} lg={0.5} />
+				<Grid item xs={1} sm={0.5} md={0.5} lg={0.5} />
+
+				<Grid item xs={2} sm={1} md={0.75} lg={0.75} >
+					<ImagenLogoHelice src={helice} />
+					{/*<img src={helice} style={{width: '50%', height: 'auto', marginLeft: '-0.5em'}} align="left"/>*/}
+					{/*<FontAwesomeIcon icon={faSailboat} size="3x" />*/}
+				</Grid>
+				<Grid item xs={8} sm={3.5} md={2} lg={2} >
+					<SpanTituloBulletpoint align={'left'} >
+						<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
+							{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto4Texto')}
+						</Typography>
+					</SpanTituloBulletpoint>
+				</Grid>
+				<Grid item xs={1} sm={1} md={2.5} lg={2.5} />
 
 
 
-				<Grid item xs={3} sm={3} md={5} lg={5} />
+				<Grid item xs={1} sm={1} md={2} lg={2} />
+				<Grid item xs={2} sm={1} md={0.75} lg={0.75} >
+					<ImagenLogoJetski src={jetSki} />
+					{/*<img src={jetSki} style={{width: '80%', height: 'auto', marginLeft: '-1.5em'}} align="left"/>*/}
+				</Grid>
+				<Grid item xs={8} sm={3.5} md={2} lg={2} >
+					<SpanTituloBulletpoint align={'left'}>
+						<Typography variant="body1" style={{fontFamily: 'Reem Kufi Fun', fontWeight: '700'}} align={'left'}  >
+							{ t('cursos.LicenciaNavegacion.Bulletpoint.Punto5Texto')}
+						</Typography>
+					</SpanTituloBulletpoint>
+				</Grid>
+
+				<Grid item xs={1} sm={0.5} md={0.5} lg={0.5} />
+				<Grid item xs={1} sm={0.5} md={0.5} lg={0.5} />
+
+				<Grid item xs={12} sm={6} md={6} lg={6} />
+
 
 
 				<Grid item xs={12} sm={12} md={12} lg={12} >
-					<div className={classes.bigSeparator} />
+					<DivBigSeparator />
 				</Grid>
+			</Grid>
 
-				{/*<Grid item xs={12} sm={12} md={12} lg={12} >*/}
-				{/*	<div className={classes.separator} />*/}
-				{/*</Grid>*/}
-
+			<Grid container spacing={0.5}>
 
 
-				<Grid item xs={12} sm={12} md={12} lg={12} >
-					<div className={classes.button}>
+
+
+			<Grid item xs={12} sm={12} md={12} lg={12} >
+					<DivBoton>
 						{/*<a*/}
 						{/*	href={`https://api.whatsapp.com/send?phone=34624158428&amp;text=${t('cursos.LicenciaNavegacion.Reserva')}`}*/}
 						{/*	target="_blank"*/}
 						{/*	rel="noopener noreferrer"*/}
 						{/*	>*/}
-							<Button size='large' variant='outlined' sx={{ bgcolor: '#CF9500', color: 'white' }} onClick={handleClickOpen}>
+						{/*	<Button size='large' variant='outlined' sx={{ bgcolor: '#CF9500', color: 'white'}} onClick={handleClickOpen}>*/}
+							<Button size='large'
+														 sx={{
+														 	bgcolor: '#CF9500',
+														 	color: 'white',
+															"&.MuiButtonBase-root:hover": {
+																bgcolor: "#CF9500",
+																transform: 'scale(1.15)',
+															}}}
+														 onClick={handleClickOpen}>
 								{ t('cursos.LicenciaNavegacion.ReservaBoton') }
 							</Button>
 						{/*</a>*/}
-					</div>
+					</DivBoton>
 				</Grid>
+				<Grid item xs={12} sm={12} md={12} lg={12} >
+					<DivSeparator />
+				</Grid>
+				<Grid item xs={12} sm={12} md={12} lg={12} >
+					<VentanaVideo embedId={'https://player.vimeo.com/video/838956949?badge=0'} />
+				</Grid>
+				<Grid item xs={12} sm={12} md={12} lg={12} >
+					<DivSeparator />
+				</Grid>
+
+{/*				<Grid item xs={12} sm={12} md={3} lg={3} >*/}
+{/*					<div height="3000px">*/}
+{/*					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />*/}
+{/*					</div>*/}
+{/*				</Grid>*/}
+{/*				<Grid item xs={12} sm={12} md={3} lg={3} >*/}
+{/*					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />*/}
+{/*				</Grid>*/}
+
+{/*<Grid item xs={12} sm={12} md={3} lg={3} >*/}
+{/*					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />*/}
+{/*				</Grid>*/}
+
+{/*<Grid item xs={12} sm={12} md={3} lg={3} >*/}
+{/*					<VentanaVideo  embedId='https://player.vimeo.com/video/835483432?h=8c9cd503a9' />*/}
+{/*				</Grid>*/}
 
 
 
